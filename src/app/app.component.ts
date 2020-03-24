64aaff3f00a82338c99590d1a37194c64263005d
Salim Chemes <salim@mobomo.com>
2020-03-24 15:47:06 -0300

    services and global error handler

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'global-error-handler';
+  title = "global-error-handler";
+
+  constructor(private http: HttpClient) {}
+
+  getClientError() {
+    let foo: any;
+    foo.fakeProperty = 1;
+    // throw new Error("My client side error");
+  }
+
+  getHttpError() {
+    this.http.get("fakeurl").subscribe();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule, ErrorHandler } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { GlobalErrorHandler } from "./services/global-error-handler";
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
+  providers: [
+    { provide: ErrorHandler, useClass: GlobalErrorHandler }
+    // {
+    //   provide: HTTP_INTERCEPTORS,
+    //   useClass: ServerErrorInterceptor,
+    //   multi: true
+    // }
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
